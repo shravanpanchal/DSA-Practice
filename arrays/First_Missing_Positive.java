e5359e4c2c23234a0dbf21eaf767c919dfda71f9
Shravan Panchal <shravanpanchal07@gmail.com>
2026-09-14 22:23:09 +0530

    First Missing Positive

--- arrays/First_Missing_Positive.java
+++ arrays/First_Missing_Positive.java
@@ -43,4 +43,44 @@ public class First_Missing_Positive {
 		return array.length+1;
 		
 	}
+	
+	public static int firstMissingPositiveOptimal(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			while (array[i] > 0 && array[i] <= array.length && array[array[i] - 1] != array[i]) {
+	            int correctIndex = array[i] - 1;
+
+				int temp = array[i];
+				array[i] = array[correctIndex];
+				array[correctIndex] = temp;
+			}
+			
+		}
+	
+			for (int i = 0; i < array.length; i++) {
+			if (array[i] != i + 1) {
+				return i + 1;
+			}
+			
+		}
+
+		return array.length + 1;
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {5, 1, -1, 2, 6,5,3};
+		System.out.println("Brute Froce Approach");
+		System.out.println("Space Complexity : O(1), Time Complexity : O(n^2)");
+		System.out.println(firstMissingPositiveBrute(nums));
+
+		System.out.println("Hashing Approach");
+		System.out.println("Space Complexity : O(n), Time Complexity : O(n)");
+		System.out.println(firstMissingPositiveHash(nums));
+
+		System.out.println("Optimal Approach");
+		System.out.println("Space Complexity : O(1), Time Complexity : O(n)");
+		System.out.println(firstMissingPositiveOptimal(nums));
+		
+
+	}
 }
